189365e4188ac2b13744d43f83a1845f3463e331
Muhammad Qadir <mahdimq@gmail.com>
2020-05-22 12:51:59 +0300

    added logic and some styling

---
+++ app.js
@@ -0,0 +1,43 @@
+const form = document.querySelector(".form")
+const displayMeme = document.querySelector(".display")
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  createMemeDiv()
+  form.reset()
+})
+
+// Remove meme when clicked on
+displayMeme.addEventListener('click', () => {
+  displayMeme.remove()
+})
+
+// combine meme logic to create a meme div
+const createMemeDiv = () => {
+  const meme = document.createElement("div")
+  displayMeme.appendChild(meme)
+  meme.className = "meme"
+
+  // Add image src and set it as background
+  const urlInput = document.querySelector("input[name=meme-url]").value
+  const memeImg = document.createElement("img")
+  memeImg.className = "img"
+  memeImg.setAttribute("src", urlInput)
+
+  // Add top text and add classname
+  const topInput = document.querySelector("input[name=top-text]").value
+  const topText = document.createElement("h2")
+  topText.className = 'top'
+  topText.innerText = topInput
+
+  // Add bottom text and add classname
+  const bottomText = document.createElement("h2")
+  const bottomInput = document.querySelector("input[name=bottom-text]").value
+  bottomText.className = 'bottom'
+  bottomText.innerText = bottomInput
+
+  // Append to the meme div
+  meme.appendChild(memeImg)
+  meme.appendChild(topText)
+  meme.appendChild(bottomText)
+}
